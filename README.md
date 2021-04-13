6ed44aa510800ddc6f97c7eee309605417334924
Javier Gracia-Tabuenca <javier.graciatabuenca@tuni.fi>
2021-04-13 15:47:51 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+
+# LongitudinalDummyDataGenerator
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of LongitudinalDummyDataGenerator is to ...
+
+## Installation
+
+You can install the released version of LongitudinalDummyDataGenerator from [CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("LongitudinalDummyDataGenerator")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(LongitudinalDummyDataGenerator)
+## basic example code
+```
+
---
+++ dev/1_start.R
@@ -0,0 +1,22 @@
+########################################
+#### CURRENT FILE: ON START SCRIPT #####
+########################################
+
+## Fill the DESCRIPTION ----
+usethis::use_description(
+  fields = list(
+    Title = "Longitudinal Dummy Data Generator",
+    Description = "Creates Longitudinal Dummy data from summary statiscics",
+    `Authors@R` = 'person("Javier", "Gracia-Tabuenca", email = "javier.graciatabuenca@tuni.fi",
+                          role = c("aut", "cre"),
+                          comment = c(ORCID = "YOUR-ORCID-ID"))',
+    License = "none",
+    Language =  "en"
+  )
+)
+
+
+usethis::use_readme_md()
+
+
+#usethis::use_git()
