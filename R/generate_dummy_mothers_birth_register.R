9f07e2c759ff9d5c1eecdfa84bb16bf94d54ddf8
shanmukhasampath <shanmukhasampath@gmail.com>
2023-04-18 14:18:35 +0300

    Added birth_mother dummy data generator function. TEST works

---
+++ R/generate_dummy_mothers_birth_register.R
@@ -0,0 +1,45 @@
+
+
+#' @title birth_mother
+#' @description FUNCTION_DESCRIPTION
+#' @param person_level_data_version PARAM_DESCRIPTION, Default: 'R10v1'
+#' @param birth_mother_level_data_version PARAM_DESCRIPTION, Default: 'R10v3'
+#' @param n_patients_minimum PARAM_DESCRIPTION, Default: 100
+#' @param seed PARAM_DESCRIPTION, Default: 13
+#' @param birth_mother PARAM_DESCRIPTION, Default: NULL
+#' @return list of created tables
+#' @export
+#' @importFrom dplyr mutate arrange desc distinct transmute left_join if_else select rename
+#' @importFrom lubridate as_date dyears year
+generate_dummy_mothers_birth_register_data <- function(
+    person_level_data_version="R10v1",
+    birth_mother_level_data_version="R10v3",
+    n_patients_minimum = 100,
+    seed=13,
+    birth_mother_data = NULL
+) {
+
+  #set seed
+  set.seed(seed)
+  # load data
+  data("summary_data_versions_list", package = "LongitudinalDummyDataGenerator")
+
+  # Person level information to match for SEX variable in FUTURE. IT IS NEEDED NOW
+  #summary_tables <- summary_data_versions_list[[person_level_data_version]]
+  # covariates has almost all the info, hence we use it to produces the minimum extended
+  #covariates_data <- scanReportToTibble(summary_tables$covariates$ScanReport_covaraites, n_patients_minimum)
+
+  # process birth_mother data
+  birth_tables <- summary_data_versions_list[[birth_mother_level_data_version]]
+  birth_mother_data <- scanReportToTibble(birth_tables$birth_mother$ScanReport_birth_mother, n_patients_minimum)
+
+  # Change the column from FINNGENID to MOTHER_FINNGENID
+  birth_mother_data <- birth_mother_data |> dplyr::rename("MOTHER_FINNGENID" = "FINNGENID")
+
+  return(birth_mother_data)
+
+}
+
+
+
+
